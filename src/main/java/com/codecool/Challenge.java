0cf93d1c19fbd345ad9df53b2d9074f23ac929ba
Misi <misi.magenheim@gmail.com>
2021-03-26 11:32:56 +0100

    algo final 1.0

--- src/main/java/com/codecool/Challenge.java
+++ src/main/java/com/codecool/Challenge.java
@@ -1,8 +1,19 @@
 package com.codecool;
 
 public class Challenge {
-
     public static int minPositive(int a, int b) {
+        int result = 0;
+        if (a > 0 && b > 0) {
+            if (a < b) {
+                result = a;
+            } else result = b;
+            } else if (a > 0 && b <= 0) {
+            result = a;
+        } else if (b > 0 && a <= 0) {
+            result = b;
+        }
+        else result = -1;
 
+        return result;
     }
 }
